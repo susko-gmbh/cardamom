13802e4224da0be5adc98f58c1066839b60794e8
sabbirsami <smd71430@gmail.com>
2025-08-07 15:43:09 +0600

    add faq section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import AngebotSection from '@/components/home/AngebotSection';
 import Banner from '@/components/home/Banner';
 import ContactSection from '@/components/home/ContactSection';
+import FAQSection from '@/components/home/FAQSection';
 import Location from '@/components/home/Loaction';
 import CardamomRestaurant from '@/components/home/restaurant/CardamomRestaurant';
 import RestaurantReservation from '@/components/home/RestaurantReservation';
@@ -14,6 +15,7 @@ export default function Home() {
       <AngebotSection />
       <RestaurantReservation />
       <ContactSection />
+      <FAQSection />
     </main>
   );
 }
