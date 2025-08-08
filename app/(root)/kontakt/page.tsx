b9994b608ca6e40ecca7b0db16b68047c91ba336
sabbirsami <smd71430@gmail.com>
2025-08-08 19:33:13 +0600

    contact page done

---
+++ app/(root)/kontakt/page.tsx
@@ -0,0 +1,11 @@
+import ContactSection from '@/components/home/ContactSection';
+
+const ContactMainPage = () => {
+  return (
+    <main className="">
+      <ContactSection />
+    </main>
+  );
+};
+
+export default ContactMainPage;
